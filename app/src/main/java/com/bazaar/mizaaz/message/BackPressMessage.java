b78126b2ed6ad6967efe63242020b75c1f55bed1
deepakbhatia <deepak.bhatia@yandex.com>
2016-12-21 12:38:28 +0100

    Addded Missing files

---
+++ app/src/main/java/com/bazaar/mizaaz/message/BackPressMessage.java
@@ -0,0 +1,14 @@
+package com.bazaar.mizaaz.message;
+
+/**
+ * Created by obelix on 17/12/2016.
+ */
+
+public class BackPressMessage {
+
+    public boolean backPress;
+
+    public BackPressMessage(boolean backPress){
+        this.backPress = backPress;
+    }
+}
